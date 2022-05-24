fd9ad49bd7c3e1469c72717813e8f436a02a21ac
“Satyam@141026” <satyam@141026@gmail.com>
2022-05-24 11:52:30 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [data ,setData]=useState("satyam")
+  let myfunction=(e)=>{
+
+console.log(data);
+
+  }
   return (
     <div className="App">
-     <p>I Love you Baby</p>
+  <form>
+      <label>Enter your name:
+        <input type="text" value={data} onChange={(e)=>{setData(e.target.value)}} />
+      </label>
+      <button type="button" name="student" onClick={myfunction} >Click here</button>
+    </form>
     </div>
   );
 }
